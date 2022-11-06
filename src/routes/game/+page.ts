5104adab203002e9508ee79b8e6055d49d7f84d6
Jani Willman <jani@willman.dev>
2022-11-06 20:29:12 +0200

    Fixed static image url

--- src/routes/game/+page.ts
+++ src/routes/game/+page.ts
@@ -10,7 +10,7 @@ export type GameData = {
 
 export function load() {
 	return {
-		url: '/src/lib/images/Super_Mario_64_box_cover.jpg',
+		url: '/images/Super_Mario_64_box_cover.jpg',
 		title: 'Super Mario 64'
 	} as GameData;
 }
