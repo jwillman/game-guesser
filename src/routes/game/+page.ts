7c5d797637b00307d927e30f91b39ec544743a63
Jani Willman <jani@willman.dev>
2022-11-06 20:05:45 +0200

    Cloudflare adapters

--- src/routes/game/+page.ts
+++ src/routes/game/+page.ts
@@ -3,6 +3,10 @@ export type GameData = {
 	title: string;
 };
 
+export async function post({ request, platform }) {
+	const counter = platform.env.COUNTER.idFromName('A');
+}
+
 export function load() {
 	return {
 		url: '/src/lib/images/Super_Mario_64_box_cover.jpg',
